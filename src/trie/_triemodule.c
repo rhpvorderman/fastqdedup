7ba9f295c1aa63b87473c9bdc304cc3469a3267a
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-04-12 08:28:05 +0200

    Add recursive function for adding sequences

--- src/trie/_triemodule.c
+++ src/trie/_triemodule.c
@@ -18,7 +18,7 @@ typedef struct {
     n->alphabet_size & TRIE_NODE_SUFFIX_SIZE_MASK)
 #define _TrieNode_SET_SUFFIX_SIZE(n, s) (n->alphabet_size = TRIE_NODE_TERMINAL_FLAG & s)
 #define TrieNode_GET_SUFFIX(n) (assert (TrieNode_IS_TERMINAL(n)), \
-    (char *)n->children)
+    (uint8_t *)n->children)
 
 static inline TrieNode * 
 TrieNode_GetChild(TrieNode * parent, size_t index) {
@@ -47,6 +47,29 @@ TrieNode_New(uint32_t alphabet_size) {
     return new; 
 }
 
+static TrieNode * 
+TrieNode_Resize(TrieNode * trie_node, uint32_t alphabet_size) {
+    if (alphabet_size > TRIE_NODE_ALPHABET_MAX_SIZE) {
+        PyErr_SetString(PyExc_SystemError, "TrieNode resized with excessive alphabet size");
+        return NULL;
+    }
+    if (alphabet_size == trie_node->alphabet_size) {
+        return trie_node;
+    }
+    size_t old_alphabet_size = TrieNode_IS_TERMINAL(trie_node) ? 0 : trie_node->alphabet_size;
+    size_t new_size = sizeof(TrieNode) + sizeof(TrieNode *) * alphabet_size;
+    TrieNode * new_node = PyMem_Realloc(trie_node, new_size);
+    if (new_node == NULL) {
+        return PyErr_NoMemory();
+    }
+    new_node->alphabet_size = alphabet_size;
+    if (alphabet_size > old_alphabet_size) {
+        size_t empty_size = (alphabet_size - old_alphabet_size) * sizeof(TrieNode *);
+        memset(new_node->children + old_alphabet_size, 0, empty_size);
+    }
+    return new_node;
+}
+
 static TrieNode *
 TrieNode_NewLeaf(uint8_t * suffix, uint32_t suffix_size) {
     if (suffix_size > TRIE_NODE_SUFFIX_MAX_SIZE) {
@@ -64,25 +87,56 @@ TrieNode_NewLeaf(uint8_t * suffix, uint32_t suffix_size) {
     return new;
 }
 
-static uint8_t
+static int
 TrieNode_AddSequence(TrieNode * trie_node, 
                      uint8_t * sequence, 
-                     size_t sequence_size, 
+                     uint32_t sequence_size, 
                      uint8_t *alphabet_size, 
                      uint8_t * charmap) {
     if (sequence_size == 0) {
         return 0;
     }
+
+    if (TrieNode_IS_TERMINAL(trie_node)) {
+        uint32_t suffix_size = TrieNode_GET_SUFFIX_SIZE(trie_node);
+        if (sequence_size == suffix_size) {
+            if (memcmp(TrieNode_GET_SUFFIX(trie_node), sequence, sequence_size) == 0){
+                trie_node->count += 1;
+                return 0;
+            }
+        }
+        uint8_t * suffix = TrieNode_GET_SUFFIX(trie_node);
+        uint8_t * tmp = PyMem_Malloc(suffix_size);
+        if (tmp == NULL) {
+            PyErr_NoMemory();
+            return -1;
+        }
+        memcpy(tmp, suffix, suffix_size);
+        TrieNode_Resize(trie_node, alphabet_size);
+        int ret = TrieNode_AddSequence(trie_node, tmp, suffix_size, alphabet_size, charmap);
+        PyMem_Free(tmp);
+        if (ret != 0){
+            return ret;
+        }
+    }
+
     uint8_t character = sequence[0];
     uint8_t node_character = charmap[character];
     if (node_character == 255) {
         charmap[character] = *alphabet_size;
         *alphabet_size = *alphabet_size + 1;
     }
-
-
-    if (TrieNode_IS_TERMINAL(trie_node)) {
-        uint32_t suffix_size = TrieNode_GET_SUFFIX_SIZE(trie_node);
-
+    if (node_character >= trie_node->alphabet_size) {
+        TrieNode_Resize(trie_node, node_character + 1);
     }
+    TrieNode * next_node = trie_node->children[node_character];
+    if (next_node == NULL) {
+        next_node = TrieNode_NewLeaf(sequence + 1, sequence_size -1);
+        if (next_node == NULL) {
+            return -1;
+        }
+        trie_node->children[node_character] = next_node;
+        return 0;
+    }
+    return TrieNode_AddSequence(next_node, sequence + 1, sequence_size - 1, alphabet_size, charmap);
 }
