4ea75baf7cf3f155af18f1f9c70df4075d1115c2
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-04-13 13:28:05 +0200

    Start on argument parser

--- src/umidedup/__init__.py
+++ src/umidedup/__init__.py
@@ -1,8 +1,26 @@
+import argparse
 import sys
+from typing import List
 
 import dnaio
 
 from ._trie import Trie
+
+
+def argument_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "fastq", metavar="FASTQ", nargs="+",
+        help="Forward FASTQ and optional reverse and UMI FASTQ files."
+    )
+    parser.add_argument(
+        "--check-lengths",
+        help="Comma-separated string with the maximum string check length of "
+             "each file. For example "
+             "``umidedup --check-lengths 16,8 R1.fastq R2.fastq`` only checks "
+             "the first 16 bases of R1 and the first 8 bases of fastq for "
+             "duplication.")
+    return parser
 
 
 def main():
