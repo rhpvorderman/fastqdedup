5bae9a9e083bdca21fb45d05c47136e38f8be5e8
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-04-14 07:54:56 +0200

    Proper command line interface

--- src/umidedup/__init__.py
+++ src/umidedup/__init__.py
@@ -1,10 +1,61 @@
 import argparse
-import sys
-from typing import List
+import contextlib
+import functools
+from typing import List, Iterable, Iterator, Optional, Tuple
 
 import dnaio
 
+import xopen
+
 from ._trie import Trie
+
+DEFAULT_PREFIX="umidedup_R"
+
+
+def file_to_fastq_reader(filename: str) -> Iterator[dnaio.SequenceRecord]:
+    opener = functools.partial(xopen.xopen, threads=0)
+    with dnaio.open(filename, mode="r", opener=opener) as fastqreader:
+        yield from fastqreader
+
+
+def _key_from_records(records: Iterable[dnaio.SequenceRecord],
+                      check_lengths: Optional[Iterable[int]]):
+    """
+    Creates a key from several records. This is an internal function.
+    This function does not check if records and check_lengths have the same
+    length.
+    """
+    if check_lengths:
+        return "".join(record.sequence[:length] for record, length in zip(records, check_lengths))
+    return "".join(record.sequence for record in records)
+
+
+def deduplicate(input_files: List[str],
+                output_files: List[str],
+                check_lengths: Optional[List[int]],
+                max_distance: int = 0):
+    if len(input_files) != len(output_files):
+        raise ValueError(f"Amount of output files ({len(output_files)}) "
+                         f"must be equal to the amount of input files "
+                         f"({len(input_files)}). ")
+    if check_lengths and len(input_files) != len(check_lengths):
+        raise ValueError(f"Amount of check lengths ({len(check_lengths)}) "
+                         f"must be equal to the amount of input files "
+                         f"({len(input_files)}). ")
+    input_readers = [file_to_fastq_reader(f) for f in input_files]
+    output_stack = contextlib.ExitStack()
+    output_opener = functools.partial(xopen.xopen, mode="wb",
+                                      compresslevel=1, threads=0)
+    output_files: List[xopen.PipedCompressionWriter] = [
+        output_stack.enter_context(output_opener(x)) for x in output_files]
+    trie = Trie()
+    for records in zip(*input_readers):  # type: Tuple[dnaio.SequenceRecord, ...]
+        key = _key_from_records(records, check_lengths)
+        if trie.sequence_present_hamming(key, max_distance):
+            continue
+        trie.add_sequence(key)
+        for record, output in zip(records, output_files):
+            output.write(record.fastq_bytes())
 
 
 def argument_parser() -> argparse.ArgumentParser:
@@ -20,18 +71,35 @@ def argument_parser() -> argparse.ArgumentParser:
              "``umidedup --check-lengths 16,8 R1.fastq R2.fastq`` only checks "
              "the first 16 bases of R1 and the first 8 bases of fastq for "
              "duplication.")
+    parser.add_argument(
+        "-o", "--output", action="append", required=False,
+        help="Output file, must be specified multiple times for multiple "
+             "input files. For example ``umidedup -o dedupR1.fastq "
+             "-o dedupR2.fastq R1.fastq R2.fastq``.")
+    parser.add_argument("-p", "--prefix", default=DEFAULT_PREFIX,
+                        help=f"Prefix for the output files. "
+                             f"Default: '{DEFAULT_PREFIX}'")
+    parser.add_argument("-d", "--max-distance", type=int, default=0,
+                        help="The Hamming distance at which inputs are "
+                             "considered different.")
     return parser
 
 
 def main():
-    trie = Trie()
-    with dnaio.open(sys.argv[1], mode="r") as fastq_reader:
-        for record in fastq_reader:
-            sequence = record.sequence
-            if not trie.sequence_present_hamming(sequence, 2):
-                trie.add_sequence(sequence)
-            else:
-                print(f"Filtered: {sequence}")
+    args = argument_parser().parse_args()
+    input_files: List[str] = args.fastq
+    if args.check_lengths:
+        check_lengths = [int(x) for x in args.check_lengths.split(",")]
+    else:
+        check_lengths = None
+    if args.output:
+        output_files = args.output
+    else:
+        output_files = [args.prefix + str(x) + ".fastq.gz"
+                        for x in range(1, len(input_files) + 1)]
+    max_distance = args.max_distance
+    deduplicate(input_files, output_files, check_lengths, max_distance)
+
 
 if __name__ == "__main__":
     main()
