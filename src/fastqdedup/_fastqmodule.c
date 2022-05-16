e6221077d8da5ddd88e78c1916b02ed4a906121d
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-05-16 15:50:49 +0200

    Start on filter functionality

--- src/fastqdedup/_fastqmodule.c
+++ src/fastqdedup/_fastqmodule.c
@@ -17,6 +17,7 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+#define MAXIMUM_PHRED_SCORE 126
 typedef struct {
     PyObject_HEAD 
     size_t total;
@@ -44,7 +45,48 @@ QualityFilter_new(PyTypeObject *type, PyObject *args, PyObject *kwargs)
     return self;
 }
 
+PyDoc_STRVAR(QualityFilter_passes_filter__doc__, 
+"passes_filter($self, phred_scores, /)\n"
+"--\n"
+"\n"
+"Check if the \n"
+"\n"
+"  phred_scores\n"
+"    ASCII string with the phred scores.\n"
+"\n"
+"Returns True if the phred_scores pass, false otherwise.\n"
+"\n");
 
+#define HAMMING_DISTANCE_METHODDEF    \
+    {"hamming_distance", (PyCFunction)(void(*)(void))hamming_distance, \
+     METH_O, QualityFilter_passes_filter__doc__}
+
+static PyObject *
+QualityFilter_passes_filter(QualityFilter *self, PyObject *phred_scores) 
+{
+    if (!PyUnicode_CheckExact(phred_scores)) {
+        PyErr_Format(PyExc_TypeError, 
+                        "phred_scores must be of type str, got %s.",
+                        Py_TYPE(phred_scores)->tp_name);
+        return NULL;
+    }
+    if (!PyUnicode_IS_COMPACT_ASCII(phred_scores)) {
+        PyErr_SetString(PyExc_ValueError, 
+                        "phred_scores must be ASCII encoded.");
+        return NULL;
+    }
+    double total_score = 0.0;
+    uint8_t *scores = PyUnicode_DATA(phred_scores);
+    uint8_t score;
+    uint8_t phred_offset = self->phred_offset;
+    Py_ssize_t length = PyUnicode_GET_LENGTH(phred_scores);
+    for (Py_ssize_t i; i<length; i+=1) {
+        score = scores[i];
+        if (score < phred_offset || score > MAXIMUM_PHRED_SCORE ) {
+            return NULL;
+        }
+    }
+}
 
 static struct PyModuleDef _fastq_module = {
     PyModuleDef_HEAD_INIT,
