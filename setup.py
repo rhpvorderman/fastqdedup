a3a0c29018d7543d8fa59ce804fc699b94ee6702
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-05-16 14:31:49 +0200

    Start on FASTQ module

--- setup.py
+++ setup.py
@@ -51,7 +51,8 @@ setup(
     install_requires=["dnaio >=0.8.0"],
     ext_modules=[
         Extension("fastqdedup._trie", ["src/fastqdedup/_triemodule.c"]),
-        Extension("fastqdedup._distance", ["src/fastqdedup/_distancemodule.c"])
+        Extension("fastqdedup._distance", ["src/fastqdedup/_distancemodule.c"]),
+        Extension("fastqdedup._fastq", ["src/fastqdedup/_fastqmodule.c"]),
     ],
     entry_points={"console_scripts": [
         "fastqdedup = fastqdedup:main"]}
