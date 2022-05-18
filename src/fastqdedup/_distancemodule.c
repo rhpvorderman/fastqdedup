b0c99806a25d228a52c0c6113039e1f3b35e59e2
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-05-18 11:08:52 +0200

    Add within_distance function

--- src/fastqdedup/_distancemodule.c
+++ src/fastqdedup/_distancemodule.c
@@ -17,6 +17,8 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+#include "distances.h"
+
 PyDoc_STRVAR(hamming_distance__doc__,
 "hamming_distance($module, string1, string2, /)\n"
 "--\n"
@@ -86,8 +88,83 @@ hamming_distance(PyObject *module,
     return PyLong_FromSsize_t(hamming_distance);
 }
 
+
+PyDoc_STRVAR(within_distance__doc__,
+"within_distance($module, string1, string2, /, max_distance, use_edit_distance=False)\n"
+"--\n"
+"\n"
+"Calculates whether two strings are within the specified distance.\n"
+"\n"
+"  string1\n"
+"    An ASCII string.\n"
+"  string2\n"
+"    Another ASCII string\n"
+"  max_distance\n"
+"     The maximum distance\n"
+"  use_edit_distance\n"
+"    Use edit (Levensteihn) distance instead of Hamming distance"
+"\n"
+"Returns an integer representing the hamming distance.\n"
+"Raises a ValueError when strings are not of the same length.\n"
+"\n");
+
+#define WITHIN_DISTANCE_METHODDEF    \
+    {"within_distance", (PyCFunction)(void(*)(void))within_distance, \
+    METH_VARARGS | METH_KEYWORDS, within_distance__doc__}
+
+PyObject *
+within_distance(PyObject *module,
+                PyObject *args,
+                PyObject *kwargs)
+{
+    PyObject *string1 = NULL;
+    PyObject *string2 = NULL;
+    int max_distance = 0;
+    int use_edit_distance = 0;
+    char *keywords = {"", "", "max_distance", "use_edit_distance", NULL};
+    char *format = "O!O!i|p:within_distance";
+    if (!PyArg_ParseTupleAndKeywords(
+            args, kwargs, format, keywords,
+            &PyUnicode_Type, &string1, &PyUnicode_Type, &string2,
+            &max_distance, &use_edit_distance)) {
+                return NULL;
+    }
+
+    if (!(PyUnicode_KIND(string1) == PyUnicode_1BYTE_KIND)) {
+        PyErr_SetString(PyExc_ValueError, 
+                        "string1 must be ASCII or latin-1 encoded.");
+        return NULL;
+    }
+    if (!(PyUnicode_KIND(string2) == PyUnicode_1BYTE_KIND)) {
+        PyErr_SetString(PyExc_ValueError, 
+                        "string2 must be ASCII or latin-1 encoded.");
+        return NULL;
+    }
+
+    uint8_t *string1chars = PyUnicode_1BYTE_DATA(string1);
+    uint8_t *string2chars = PyUnicode_1BYTE_DATA(string2);
+    Py_ssize_t string1len = PyUnicode_GET_LENGTH(string1);
+    Py_ssize_t string2len = PyUnicode_GET_LENGTH(string2);
+    ssize_t ret;
+
+    if (use_edit_distance) {
+        ret = within_edit_distance(string1chars, string1len,
+                                   string2chars, string2len, 
+                                   max_distance);
+
+    }
+    else {
+        ret = within_hamming_distance(string1chars, string1len,
+                                      string2chars, string2len, 
+                                      max_distance);
+    }
+    return PyBool_FromLong(ret);
+}
+
+
 static PyMethodDef _distance_functions[] = {
     HAMMING_DISTANCE_METHODDEF,
+    WITHIN_DISTANCE_METHODDEF,
     {NULL}
 };
 
