a67779a822037848164566896f10f5ea778389a6
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-05-18 13:40:59 +0200

    Remove unused function

--- src/fastqdedup/_distancemodule.c
+++ src/fastqdedup/_distancemodule.c
@@ -19,75 +19,6 @@
 
 #include "distances.h"
 
-PyDoc_STRVAR(hamming_distance__doc__,
-"hamming_distance($module, string1, string2, /)\n"
-"--\n"
-"\n"
-"Calculates the hamming distance between two strings.\n"
-"\n"
-"  string1\n"
-"    An ASCII string.\n"
-"  string2\n"
-"    Another ASCII string\n"
-"\n"
-"Returns an integer representing the hamming distance.\n"
-"Raises a ValueError when strings are not of the same length.\n"
-"\n");
-
-#define HAMMING_DISTANCE_METHODDEF    \
-    {"hamming_distance", (PyCFunction)(void(*)(void))hamming_distance, METH_FASTCALL, \
-     hamming_distance__doc__}
-
-PyObject *
-hamming_distance(PyObject *module,
-                 PyObject *const *args,
-                 Py_ssize_t nargs)
-{
-    if (nargs != 2) {
-        PyErr_Format(
-            PyExc_TypeError,
-            "hamming distance expects exactly two arguments, got %zd", 
-            nargs);
-        return NULL;
-    }
-    Py_ssize_t i;
-    PyObject *arg; 
-    for (i=0; i<2; i+=1) {
-        arg = args[i];
-        if (!PyUnicode_CheckExact(arg)) {
-            PyErr_Format(PyExc_TypeError, 
-                         "string%zd must be of type str, got %s.",
-                         i + 1, Py_TYPE(arg)->tp_name);
-            return NULL;
-        }
-        if (!(PyUnicode_KIND(arg) == PyUnicode_1BYTE_KIND)) {
-            PyErr_Format(PyExc_ValueError, 
-                        "string%zd must be ASCII or latin-1 encoded.", 
-                        i + 1);
-            return NULL;
-        }
-    }
-    PyObject *string1 = args[0];
-    PyObject *string2 = args[1];
-    uint8_t *string1chars = PyUnicode_1BYTE_DATA(string1);
-    uint8_t *string2chars = PyUnicode_1BYTE_DATA(string2);
-    Py_ssize_t string1len = PyUnicode_GET_LENGTH(string1);
-    Py_ssize_t string2len = PyUnicode_GET_LENGTH(string2);
-
-    if (string1len != string2len) {
-        PyErr_SetString(PyExc_ValueError, 
-                        "string1 and string2 must be of the same length");
-        return NULL;
-    }
-    Py_ssize_t hamming_distance = 0;
-    for (i=0; i<string1len; i+=1) {
-        if (string1chars[i] != string2chars[i]) {
-            hamming_distance += 1;
-        }
-    }
-    return PyLong_FromSsize_t(hamming_distance);
-}
-
 
 PyDoc_STRVAR(within_distance__doc__,
 "within_distance($module, string1, string2, /, max_distance, use_edit_distance=False)\n"
@@ -163,7 +94,6 @@ within_distance(PyObject *module,
 
 
 static PyMethodDef _distance_functions[] = {
-    HAMMING_DISTANCE_METHODDEF,
     WITHIN_DISTANCE_METHODDEF,
     {NULL}
 };
